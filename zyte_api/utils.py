af9d34d148e6be5e82355614dad6a0c539d21f5a
Adrián Chaves <adrian@chaves.io>
2022-05-17 14:43:28 +0200

    Propose a minimal specialization for extract

--- zyte_api/utils.py
+++ zyte_api/utils.py
@@ -1,5 +1,11 @@
-# -*- coding: utf-8 -*-
 from .__version__ import __version__
+
+
+def _to_lower_camel_case(snake_case_string):
+    """Convert from snake case (foo_bar) to lower-case-initial camel case
+    (fooBar)."""
+    prefix, *rest = snake_case_string.split('_')
+    return prefix + ''.join(part.title() for part in rest)
 
 
 def user_agent(library):
