b2edeac6f36f61a5714d5ed6ed7ae090bfc3c797
Mikhail Korobov <kmike84@gmail.com>
2022-10-28 16:10:07 +0500

    Bump version: 0.4.1 → 0.4.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = u'Zyte Group Ltd'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.4.1'
+release = u'0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
