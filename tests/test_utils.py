86a7767abd8c4d29799cdb708deb3334e3bf851c
PyExplorer <stgmont@gmail.com>
2023-09-24 22:54:47 +0300

    test for constant user_agent

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 import pytest
 from pytest import raises
 
-from zyte_api.utils import __version__, _guess_intype, _process_query, _user_agent
+from zyte_api.aio.client import AsyncClient
+from zyte_api.utils import USER_AGENT, _guess_intype, _process_query
 
 
 @pytest.mark.parametrize(
@@ -99,17 +100,18 @@ def test_process_query_bytes():
 
 
 @pytest.mark.parametrize(
-    "custom_user_agent,expected",
+    "user_agent,expected",
     (
         (
             None,
-            f'python-zyte-api/{__version__}',
+            USER_AGENT,
         ),
         (
-            'scrapy-zyte-api/0.11.1',
-            f'python-zyte-api/{__version__}, scrapy-zyte-api/0.11.1',
+            f'{USER_AGENT}, scrapy-zyte-api/0.11.1',
+            f'{USER_AGENT}, scrapy-zyte-api/0.11.1',
         ),
     ),
 )
-def test_user_agent(custom_user_agent, expected):
-    assert _user_agent(custom_user_agent) == expected
+def test_user_agent(user_agent, expected):
+    client = AsyncClient(api_key='123', api_url='http:\\test', user_agent=user_agent)
+    assert client.user_agent == expected
