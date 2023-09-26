ad7e3a80bbb0341434b52f2535891222ad177ac3
Shevchenko Taras <stgmont@gmail.com>
2023-09-26 12:16:37 +0300

    Allow overriding the user agent (#50)

---
+++ tests/test_client.py
@@ -0,0 +1,22 @@
+import pytest
+
+from zyte_api.aio.client import AsyncClient
+from zyte_api.utils import USER_AGENT
+
+
+@pytest.mark.parametrize(
+    "user_agent,expected",
+    (
+        (
+            None,
+            USER_AGENT,
+        ),
+        (
+            f'scrapy-zyte-api/0.11.1 {USER_AGENT}',
+            f'scrapy-zyte-api/0.11.1 {USER_AGENT}',
+        ),
+    ),
+)
+def test_user_agent(user_agent, expected):
+    client = AsyncClient(api_key='123', api_url='http:\\test', user_agent=user_agent)
+    assert client.user_agent == expected
--- zyte_api/utils.py
+++ zyte_api/utils.py
@@ -5,6 +5,8 @@ from w3lib.url import safe_url_string
 
 from .__version__ import __version__
 
+USER_AGENT = f'python-zyte-api/{__version__}'
+
 
 def _guess_intype(file_name, lines):
     _, dot_extension = splitext(file_name)
@@ -42,10 +44,3 @@ def _process_query(query):
     if url == safe_url:
         return query
     return {**query, "url": safe_url}
-
-
-def user_agent(library):
-    return 'python-zyte-api/{} {}/{}'.format(
-        __version__,
-        library.__name__,
-        library.__version__)
