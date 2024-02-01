931a8103b5dfc63f40be4e663306600c440b7fa7
Adnan Awan <adnan.awan.aa@gmail.com>
2024-02-01 17:50:53 +0500

    Provide an option to store error responses in CLI (#47)

---
+++ tests/test_main.py
@@ -0,0 +1,132 @@
+import json
+import os
+from json import JSONDecodeError
+from unittest.mock import Mock, patch, AsyncMock
+
+import pytest
+
+from zyte_api.__main__ import run
+
+
+class RequestError(Exception):
+    @property
+    def parsed(self):
+        mock = Mock(
+            response_body=Mock(decode=Mock(return_value=forbidden_domain_response()))
+        )
+        return mock
+
+
+def get_json_content(file_object):
+    if not file_object:
+        return
+
+    file_path = file_object.name
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except JSONDecodeError:
+        pass
+
+
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        print(f"File '{file_path}' has been deleted successfully.")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found. Unable to delete.")
+
+
+def forbidden_domain_response():
+    response_str = {
+        "type": "/download/temporary-error",
+        "title": "Temporary Downloading Error",
+        "status": 520,
+        "detail": "There is a downloading problem which might be temporary. Retry in N seconds from 'Retry-After' header or open a support ticket from https://support.zyte.com/support/tickets/new if it fails consistently.",
+    }
+    return response_str
+
+
+async def fake_exception(value=True):
+    # Simulating an error condition
+    if value:
+        raise RequestError()
+
+    create_session_mock = AsyncMock()
+    return await create_session_mock.coroutine()
+
+
+@pytest.mark.parametrize(
+    "queries,expected_response,store_errors,exception",
+    (
+        (
+            # test if it stores the error(s) also by adding flag
+            (
+                [
+                    {
+                        "url": "https://forbidden.example",
+                        "browserHtml": True,
+                        "echoData": "https://forbidden.example",
+                    }
+                ],
+                forbidden_domain_response(),
+                True,
+                fake_exception,
+            ),
+            # test with store_errors=False
+            (
+                [
+                    {
+                        "url": "https://forbidden.example",
+                        "browserHtml": True,
+                        "echoData": "https://forbidden.example",
+                    }
+                ],
+                None,  # expected response should be None
+                False,
+                fake_exception,
+            ),
+        )
+    ),
+)
+@pytest.mark.asyncio
+async def test_run(queries, expected_response, store_errors, exception):
+    temporary_file = open("temporary_file.jsonl", "w")
+    n_conn = 5
+    stop_on_errors = False
+    api_url = "https://example.com"
+    api_key = "fake_key"
+    retry_errors = True
+
+    # Create a mock for AsyncClient
+    async_client_mock = Mock()
+
+    # Create a mock for the request_parallel_as_completed method
+    request_parallel_mock = Mock()
+    async_client_mock.return_value.request_parallel_as_completed = request_parallel_mock
+
+    # Patch the AsyncClient class in __main__ with the mock
+    with patch("zyte_api.__main__.AsyncClient", async_client_mock), patch(
+        "zyte_api.__main__.create_session"
+    ) as create_session_mock:
+        # Mock create_session to return an AsyncMock
+        create_session_mock.return_value = AsyncMock()
+
+        # Set up the AsyncClient instance to return the mocked iterator
+        async_client_mock.return_value.request_parallel_as_completed.return_value = [
+            exception(),
+        ]
+
+        # Call the run function with the mocked AsyncClient
+        await run(
+            queries=queries,
+            out=temporary_file,
+            n_conn=n_conn,
+            stop_on_errors=stop_on_errors,
+            api_url=api_url,
+            api_key=api_key,
+            retry_errors=retry_errors,
+            store_errors=store_errors,
+        )
+
+    assert get_json_content(temporary_file) == expected_response
--- zyte_api/__main__.py
+++ zyte_api/__main__.py
@@ -1,11 +1,11 @@
 """ Basic command-line interface for Zyte API. """
 
 import argparse
-import json
-import sys
 import asyncio
+import json
 import logging
 import random
+import sys
 
 import tqdm
 from tenacity import retry_if_exception
@@ -14,45 +14,64 @@ from zyte_api.aio.client import (
     create_session,
     AsyncClient,
 )
+from zyte_api.aio.retry import RetryFactory, _is_throttling_error
 from zyte_api.constants import ENV_VARIABLE, API_URL
 from zyte_api.utils import _guess_intype
-from zyte_api.aio.retry import RetryFactory, _is_throttling_error
 
 
 class DontRetryErrorsFactory(RetryFactory):
     retry_condition = retry_if_exception(_is_throttling_error)
 
 
-logger = logging.getLogger('zyte_api')
+logger = logging.getLogger("zyte_api")
 
 _UNSET = object()
 
 
-async def run(queries, out, *, n_conn, stop_on_errors, api_url,
-              api_key=None, retry_errors=True):
+async def run(
+    queries,
+    out,
+    *,
+    n_conn,
+    stop_on_errors,
+    api_url,
+    api_key=None,
+    retry_errors=True,
+    store_errors=None,
+):
+    def write_output(content):
+        json.dump(content, out, ensure_ascii=False)
+        out.write("\n")
+        out.flush()
+        pbar.update()
+
     retrying = None if retry_errors else DontRetryErrorsFactory().build()
-    client = AsyncClient(n_conn=n_conn, api_key=api_key, api_url=api_url,
-                         retrying=retrying)
+    client = AsyncClient(
+        n_conn=n_conn, api_key=api_key, api_url=api_url, retrying=retrying
+    )
     async with create_session(connection_pool_size=n_conn) as session:
         result_iter = client.request_parallel_as_completed(
             queries=queries,
             session=session,
         )
-        pbar = tqdm.tqdm(smoothing=0, leave=True, total=len(queries), miniters=1,
-                         unit="url")
+        pbar = tqdm.tqdm(
+            smoothing=0, leave=True, total=len(queries), miniters=1, unit="url"
+        )
         pbar.set_postfix_str(str(client.agg_stats))
         try:
             for fut in result_iter:
                 try:
                     result = await fut
-                    json.dump(result, out, ensure_ascii=False)
-                    out.write("\n")
-                    out.flush()
-                    pbar.update()
                 except Exception as e:
+                    if store_errors:
+                        write_output(e.parsed.response_body.decode())
+
                     if stop_on_errors:
                         raise
+
                     logger.error(str(e))
+                else:
+                    write_output(result)
                 finally:
                     pbar.set_postfix_str(str(client.agg_stats))
         finally:
@@ -72,10 +91,7 @@ def read_input(input_fp, intype):
         urls = [u.strip() for u in lines if u.strip()]
         records = [{"url": url, "browserHtml": True} for url in urls]
     else:
-        records = [
-            json.loads(line.strip())
-            for line in lines if line.strip()
-        ]
+        records = [json.loads(line.strip()) for line in lines if line.strip()]
     # Automatically replicating the url in echoData to being able to
     # to match URLs with content in the responses
     for record in records:
@@ -83,76 +99,104 @@ def read_input(input_fp, intype):
     return records
 
 
-def _main(program_name='zyte-api'):
-    """ Process urls from input file through Zyte API """
+def _main(program_name="zyte-api"):
+    """Process urls from input file through Zyte API"""
     p = argparse.ArgumentParser(
         prog=program_name,
         description="""
         Process input URLs from a file using Zyte API.
         """,
     )
-    p.add_argument("input",
-                   type=argparse.FileType("r", encoding='utf8'),
-                   help="Input file with urls, url per line by default. The "
-                        "Format can be changed using `--intype` argument.")
-    p.add_argument("--intype", default=_UNSET, choices=["txt", "jl"],
-                   help="Type of the input file. "
-                        "Allowed values are 'txt' (1 URL per line) and 'jl' "
-                        "(JSON Lines file, each object describing the "
-                        "parameters of a request). "
-                        "If not specified, the input type is guessed based on "
-                        "the input file name extension (.jl, .jsonl, .txt) or "
-                        "content, and assumed to be txt if guessing fails.")
-    p.add_argument("--limit", type=int,
-                   help="Max number of URLs to take from the input")
-    p.add_argument("--output", "-o",
-                   default=sys.stdout,
-                   type=argparse.FileType("w", encoding='utf8'),
-                   help=".jsonlines file to store extracted data. "
-                        "By default, results are printed to stdout.")
-    p.add_argument("--n-conn", type=int, default=20,
-                   help="number of connections to the API server "
-                        "(default: %(default)s)")
-    p.add_argument("--api-key",
-                   help="Zyte API key. "
-                        "You can also set %s environment variable instead "
-                        "of using this option." % ENV_VARIABLE)
-    p.add_argument("--api-url",
-                   help="Zyte API endpoint (default: %(default)s)",
-                   default=API_URL)
-    p.add_argument("--loglevel", "-L", default="INFO",
-                   choices=["DEBUG", "INFO", "WARNING", "ERROR"],
-                   help="log level (default: %(default)s)")
-    p.add_argument("--shuffle", help="Shuffle input URLs", action="store_true")
-    p.add_argument("--dont-retry-errors",
-                   help="Don't retry request and network errors",
-                   action="store_true")
-    args = p.parse_args()
-    logging.basicConfig(
-        stream=sys.stderr,
-        level=getattr(logging, args.loglevel)
+    p.add_argument(
+        "input",
+        type=argparse.FileType("r", encoding="utf8"),
+        help="Input file with urls, url per line by default. The "
+        "Format can be changed using `--intype` argument.",
     )
+    p.add_argument(
+        "--intype",
+        default=_UNSET,
+        choices=["txt", "jl"],
+        help="Type of the input file. "
+        "Allowed values are 'txt' (1 URL per line) and 'jl' "
+        "(JSON Lines file, each object describing the "
+        "parameters of a request). "
+        "If not specified, the input type is guessed based on "
+        "the input file name extension (.jl, .jsonl, .txt) or "
+        "content, and assumed to be txt if guessing fails.",
+    )
+    p.add_argument(
+        "--limit", type=int, help="Max number of URLs to take from the input"
+    )
+    p.add_argument(
+        "--output",
+        "-o",
+        default=sys.stdout,
+        type=argparse.FileType("w", encoding="utf8"),
+        help=".jsonlines file to store extracted data. "
+        "By default, results are printed to stdout.",
+    )
+    p.add_argument(
+        "--n-conn",
+        type=int,
+        default=20,
+        help="number of connections to the API server " "(default: %(default)s)",
+    )
+    p.add_argument(
+        "--api-key",
+        help="Zyte API key. "
+        "You can also set %s environment variable instead "
+        "of using this option." % ENV_VARIABLE,
+    )
+    p.add_argument(
+        "--api-url", help="Zyte API endpoint (default: %(default)s)", default=API_URL
+    )
+    p.add_argument(
+        "--loglevel",
+        "-L",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        help="log level (default: %(default)s)",
+    )
+    p.add_argument("--shuffle", help="Shuffle input URLs", action="store_true")
+    p.add_argument(
+        "--dont-retry-errors",
+        help="Don't retry request and network errors",
+        action="store_true",
+    )
+    p.add_argument(
+        "--store-errors",
+        help="when set to true, it includes all types of responses, and when set to false,"
+             " it includes only error-free responses in the output.",
+    )
+    args = p.parse_args()
+    logging.basicConfig(stream=sys.stderr, level=getattr(logging, args.loglevel))
 
     queries = read_input(args.input, args.intype)
     if args.shuffle:
         random.shuffle(queries)
     if args.limit:
-        queries = queries[:args.limit]
+        queries = queries[: args.limit]
 
-    logger.info(f"Loaded {len(queries)} urls from {args.input.name}; shuffled: {args.shuffle}")
+    logger.info(
+        f"Loaded {len(queries)} urls from {args.input.name}; shuffled: {args.shuffle}"
+    )
     logger.info(f"Running Zyte API (connections: {args.n_conn})")
 
     loop = asyncio.get_event_loop()
-    coro = run(queries,
-               out=args.output,
-               n_conn=args.n_conn,
-               stop_on_errors=False,
-               api_url=args.api_url,
-               api_key=args.api_key,
-               retry_errors=not args.dont_retry_errors)
+    coro = run(
+        queries,
+        out=args.output,
+        n_conn=args.n_conn,
+        stop_on_errors=False,
+        api_url=args.api_url,
+        api_key=args.api_key,
+        retry_errors=not args.dont_retry_errors,
+        store_errors=args.store_errors,
+    )
     loop.run_until_complete(coro)
     loop.close()
 
 
-if __name__ == '__main__':
-    _main(program_name='python -m zyte_api')
+if __name__ == "__main__":
+    _main(program_name="python -m zyte_api")
