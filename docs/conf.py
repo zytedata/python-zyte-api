85e9ab587ba29056bd50d5f4224bef37a06e5e17
Adrián Chaves <adrian@chaves.io>
2024-04-05 14:24:27 +0200

    Bump version: 0.4.8 → 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = "Zyte Group Ltd"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.4.8"
+release = "0.5.0"
 
 
 # -- General configuration ---------------------------------------------------
