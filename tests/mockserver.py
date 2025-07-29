e3931c779a1f91bb61256d06f9a9811ac601bf25
Andrey Rakhmatullin <wrar@wrar.name>
2025-07-29 11:40:39 +0500

    Update tests/mockserver.py

--- tests/mockserver.py
+++ tests/mockserver.py
@@ -169,7 +169,7 @@ class DefaultResource(Resource):
             elif echo_data == "402-payment-retry-exceeded":
                 assert request.getHeader("X-Payment")
                 # Return 402 on the first 2 requests, then 200 on the third
-                # (the client will give up after 2 attempts, so the will be no
+                # (the client will give up after 2 attempts, so there will be no
                 # third in practice)
                 if not session_data:
                     session_data["payment_attempts"] = 1
